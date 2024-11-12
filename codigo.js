e5b3c82f348409a3dcdb19bfac682d7f1364cb85
CarlosRauda64 <rauda.carlos6432@gmail.com>
2024-11-11 18:06:04 -0600

    :sparkles: Se agrego el boton de agregar y eliminar productos, ademas se cambio la manera de aparecer de la tabla

--- codigo.js
+++ codigo.js
@@ -10,7 +10,6 @@ const btnCancelar = document.querySelector('#can-btn-forms'); // botón de cance
 const btnAgregar = document.querySelector('#add-btn-forms'); // botón de agregar
 const limpiarFiltrosBtn = document.querySelector('#limpiar-filtros'); // botón de limpiar filtros
 
-// Función para mostrar artículos
 function mostrarArticulos() {
     const main = document.querySelector('#main');
     main.classList.toggle('active');
@@ -27,11 +26,14 @@ getArticulos = async () => {
             if (isNaN(articulo.image) && isNaN(articulo.title) && isNaN(articulo.category) && isNaN(articulo.description)) {
                 const tr = document.createElement('tr');
                 tr.innerHTML = `
+            <tr>
                 <td class="container-img"><img src="${articulo.image}" alt="${articulo.title}-img"></td>
                 <td>${articulo.title}</td>
                 <td>$${articulo.price}</td>
                 <td>${articulo.category}</td>
                 <td>${articulo.description}</td>
+                <td class="container-img"><button id="btn-eliminar" onclick="obtenerId(this)" data-info="${articulo.id}"><img src="trash-icon.svg" alt="btn-eliminar"></button></td> 
+            <tr>
             `;
                 tbody.appendChild(tr);
             }
@@ -41,8 +43,90 @@ getArticulos = async () => {
     }
 }
 
-//Se invoca para colocar los articulos en la tabla
-getArticulos();
+// Evento para agregar un artículo
+btnAgregar.addEventListener('click', () => {
+    let image = document.querySelector('#product-image').value
+    let title = document.querySelector('#product-title').value
+    let price = document.querySelector('#product-price').value
+    let category = document.querySelector('#product-category').value
+    let description = document.querySelector('#product-description').value
+    // Verifica que los campos no esten vacíos
+    if (image.trim() !== '' && title.trim() !== '' && price.trim() !== '' && category.trim() !== '' && description.trim() !== '') {
+        const articulo = {
+            image: image,
+            title: title,
+            price: price,
+            category: category,
+            description: description
+        }
+        colocarArticulo(articulo).then((result) => {
+            getArticulos();
+        });
+        limpiarCampos();
+        addContainer.classList.toggle('active');
+    }
+    else {
+        alert('Por favor, ingrese los datos correctamente')
+    }
+});
+
+function limpiarCampos() {
+    document.querySelector('#product-image').value = ''
+    document.querySelector('#product-title').value = ''
+    document.querySelector('#product-price').value = ''
+    document.querySelector('#product-category').value = ''
+    document.querySelector('#product-description').value = ''
+}
+
+// Función para colocar un artículo
+colocarArticulo = async (articulo) => {
+    try {
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(articulo)
+        });
+        const data = await response.json();
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
+// Evento para mostrar/ocultar el contenedor de agregar
+btnAdd.addEventListener('click', () => {
+    addContainer.classList.toggle('active');
+});
+
+// Evento para cancelar la acción de agregar
+btnCancelar.addEventListener('click', () => {
+    addContainer.classList.toggle('active');
+    limpiarCampos();
+});
+
+// Obtiene los artículos al cargar la página
+let articulos = getArticulos();
+
+// Función para obtener el ID del artículo a eliminar
+function obtenerId(button) {
+    const contenido = button.getAttribute('data-info');
+    eliminarArticulo(contenido).then((result) => {
+        filtrarArticulos();
+    });
+}
+
+// Función para eliminar un artículo
+eliminarArticulo = async (id) => {
+    try {
+        const response = await fetch(`${endpoint}/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
 
 // Función para filtrar los articulos
 filtrarArticulos = async () => {
@@ -68,11 +152,14 @@ filtrarArticulos = async () => {
             if (isNaN(articulo.image) && isNaN(articulo.title) && isNaN(articulo.category) && isNaN(articulo.description)) {
                 const tr = document.createElement('tr');
                 tr.innerHTML = `
+            <tr>
                 <td class="container-img"><img src="${articulo.image}" alt="${articulo.title}-img"></td>
                 <td>${articulo.title}</td>
                 <td>$${articulo.price}</td>
                 <td>${articulo.category}</td>
                 <td>${articulo.description}</td>
+                <td class="container-img"><button id="btn-eliminar" onclick="obtenerId(this)" data-info="${articulo.id}"><img src="trash-icon.svg" alt="btn-eliminar"></button></td> 
+            <tr>
             `;
                 tbody.appendChild(tr);
             }
